b6e293c3881deee6fb44065bd33b14d6fe03b901
willer-barros <willerbarros137@gmail.com>
2024-09-08 12:04:52 -0300

    validacoes

--- src/main/java/med/vol/api/controller/MedicoController.java
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -1,6 +1,7 @@
 package med.vol.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ public class MedicoController {
     private MedicoRepository repository;
 
     @PostMapping
+    @Transactional    // tem de importar o do spring
     public void cadastrar(@RequestBody DadosCadastroMedico dados){
         repository.save(new Medico(dados));
     }
--- src/main/java/med/vol/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/vol/api/medico/DadosCadastroMedico.java
@@ -1,9 +1,35 @@
 package med.vol.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.vol.api.endereco.DadosEndereco;
 
+//usamos o bean validation
+//@notblank ja faz a validacao se o campo eh null
+
+public record DadosCadastroMedico(
+
+@NotBlank
+String nome,
+
+@NotBlank
+@Email
+String email, 
+
+@NotBlank
+@Pattern(regexp = "\\d{4,6}")
+String crm, 
+
+@NotNull
+Especialidade especialidade, 
 
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+@NotNull
+@Valid 
+DadosEndereco endereco
+) {
 
 }
