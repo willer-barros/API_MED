6f213c7ff45fced4f1add47109958fcf1afe1123
willer-barros <willerbarros137@gmail.com>
2024-09-08 19:37:31 -0300

    ordenacao

--- src/main/java/med/vol/api/controller/MedicoController.java
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -3,6 +3,7 @@ package med.vol.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +28,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(Pageable paginacao){
+    public Page<DadosListagemMedico> listar(@PageableDefault(size=10, sort = {"nome"})  Pageable paginacao){
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
     
