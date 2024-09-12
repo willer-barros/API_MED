209e1e13bf6b20a58f21f65be7e419a1b95ac5a0
willer-barros <willerbarros137@gmail.com>
2024-09-11 23:25:57 -0300

    metodo PUT

--- src/main/java/med/vol/api/controller/MedicoController.java
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -7,6 +7,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +31,14 @@ public class MedicoController {
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size=10, sort = {"nome"})  Pageable paginacao){
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
+
+    
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
     }
     
 }
---
+++ src/main/java/med/vol/api/medico/DadosAtualizacaoMedico.java
@@ -0,0 +1,18 @@
+package med.vol.api.medico;
+
+import jakarta.validation.constraints.NotNull;
+import med.vol.api.endereco.DadosEndereco;
+
+public record DadosAtualizacaoMedico(
+
+@NotNull
+Long id,
+
+String nome,
+
+String telefone,
+
+DadosEndereco endereco) {
+
+    
+}
